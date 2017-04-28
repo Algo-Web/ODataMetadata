be48d78670809de4abe6bb2c3ddf6226545a55e5
c-harris <c.harris@communicationinnovations.com.au>
2017-04-28 11:30:57 +0000

    Apply fixes from StyleCI

--- IsOK.php
+++ IsOK.php
@@ -5,52 +5,55 @@ abstract class IsOK
 {
     abstract protected function IsOK(&$msg);
 
-    protected function isStringNotNullOrEmpty($str){
-        if($this->isStringNull($str)){
+    protected function isStringNotNullOrEmpty($str)
+    {
+        if ($this->isStringNull($str)) {
             return false;
         }
-        if(empty(trim($str))){
-           return false;
+        if (empty(trim($str))) {
+            return false;
         }
         return true;
     }
 
     protected function isStringNotNull($str)
     {
-        if(null == $str){
-           return false;
-        }
-        if(!is_string($str)){
-           return false;
-        }
-        return true;
-    }
-
-    protected  function isNotNullIstanceOf($var, $insanceOf){
-        if(null == $var){
+        if (null == $str) {
             return false;
         }
-        if(!($var instanceof $insanceOf)){
+        if (!is_string($str)) {
             return false;
         }
         return true;
     }
 
-    protected function isValidArray($arr, $insanceOf, $minCount = -1, $maxCount = -1){
-        if(null == $arr){
+    protected function isNotNullIstanceOf($var, $insanceOf)
+    {
+        if (null == $var) {
             return false;
         }
-        if(!is_array ($arr)){
+        if (!($var instanceof $insanceOf)) {
             return false;
         }
-        if($minCount != -1 && count($arr) < $minCount){
+        return true;
+    }
+
+    protected function isValidArray($arr, $insanceOf, $minCount = -1, $maxCount = -1)
+    {
+        if (null == $arr) {
             return false;
         }
-        if($maxCount != -1 && count($arr) > $maxCount){
+        if (!is_array($arr)) {
             return false;
         }
-        foreach($arr as $item){
-            if(!($item instanceof $insanceOf)){
+        if ($minCount != -1 && count($arr) < $minCount) {
+            return false;
+        }
+        if ($maxCount != -1 && count($arr) > $maxCount) {
+            return false;
+        }
+        foreach ($arr as $item) {
+            if (!($item instanceof $insanceOf)) {
                 return false;
             }
         }
