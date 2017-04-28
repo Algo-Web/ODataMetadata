fd393424dad96e7f8151bf2a983cc5052847487e
c-harris <c.harris@communicationinnovations.com.au>
2017-04-28 18:41:31 +1000

    added IsOK AbstractClass

---
+++ IsOK.php
@@ -0,0 +1,53 @@
+<?php
+namespace AlgoWeb\ODataMetadata
+
+abstract class IsOK
+{
+    abstract protected function IsOK(&$msg);
+
+    public protected function isStringNotNullOrEmpty($str){
+        if($this->isStringNull($str)){
+            return false;
+        }
+        if(empty(trim($str))){
+           return false;
+        }
+        return true;
+    }
+
+    public protected function isStringNotNull($str)
+    {
+        if(null == $str){
+           return false;
+        if(!is_string($str)){
+           return false;
+        }
+        return true;
+    }
+
+    public protected  function isNotNullIstanceOf($var, $insanceOf){
+        if(null == $var){
+            return false;
+        }
+        if(!($var instanceof $insanceOf)){
+            return false;
+        }
+        return true;
+    }
+
+    public protected function isValidArray($arr, $insanceOf, $minCount = -1, $maxCount = -1){
+        if(null == $arr){
+            return false;
+        }
+        if(!is_array ($arr)){
+            return false;
+        }
+        if($minCount != -1 && count($arr) < $minCount){
+            return false;
+        }
+        if($maxCount != -1 && count($arr) > $maxCount){
+            return false;
+        }
+        return true;
+    }
+}
