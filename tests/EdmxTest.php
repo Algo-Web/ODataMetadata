c6e89f6fd76204cf383e8cf32acc6ee22350cbbf
Chris <c.harris@communicationinnovations.com.au>
2017-05-09 20:00:58 +1000

    Added check against the xsd

--- tests/EdmxTest.php
+++ tests/EdmxTest.php
@@ -12,4 +12,26 @@ class EdmxTest extends TestCase
         $edmx = new Edmx();
         $this->assertTrue($edmx->isOK($msg), $msg);
     }
+
+    public function testDefaultSerializeOk()
+    {
+        $msg = null;
+        $edmx = new Edmx();
+        $this->assertTrue($edmx->isOK($msg), $msg);
+        $ymlDir = dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "MetadataV3" . DIRECTORY_SEPARATOR . "JMSmetadata";
+        $serializer =
+            \JMS\Serializer\SerializerBuilder::create()
+                ->addMetadataDir($ymlDir)
+                ->build();
+        $d = $serializer->serialize($edmx, "xml");
+        $this->V3MetadataAgainstXSD($d);
+    }
+
+    public function V3MetadataAgainstXSD($data)
+    {
+
+        $xml = new \DOMDocument();
+        $xml->loadXML($data);
+        $xml->schemaValidate(dirname(__DIR__) . DIRECTORY_SEPARATOR . "xsd" . DIRECTORY_SEPARATOR . "/Microsoft.Data.Entity.Design.Edmx_3.xsd");
+    }
 }
