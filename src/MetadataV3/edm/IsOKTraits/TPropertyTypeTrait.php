21029c4202e1aa508434f3585669cba8cd87d129
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-06-28 00:43:52 +1000

    Hoist v3 property type regex creation outside validity check

--- src/MetadataV3/edm/IsOKTraits/TPropertyTypeTrait.php
+++ src/MetadataV3/edm/IsOKTraits/TPropertyTypeTrait.php
@@ -6,11 +6,15 @@ trait TPropertyTypeTrait
 {
     use EDMSimpleTypeTrait, TQualifiedNameTrait;
 
+    protected static $v3PropertyTypeCache = [];
+    protected static $v3PropertyTypeRegex =
+        "/[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}(\.[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}){0,}/";
+
     public function isTPropertyTypeValid($string)
     {
         //The below pattern represents the allowed identifiers in ECMA specification
         // plus the '.' for namespace qualification
-        $regex = "/[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}(\.[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}){0,}/";
+        //$regex = "/[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}(\.[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}){0,}/";
 
         if (!is_string($string)) {
             $msg = "Input must be a string: " . get_class($this);
@@ -22,6 +26,6 @@ trait TPropertyTypeTrait
         if ($this->isTQualifiedNameValid($string)) {
             return true;
         }
-        return $this->matchesRegexPattern($regex, $string);
+        return $this->matchesRegexPattern(static::$v3PropertyTypeRegex, $string);
     }
 }
