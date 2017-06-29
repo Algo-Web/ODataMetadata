0a032a3f86a520aa196fe5896e2d4bc0780aabf5
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-06-30 02:49:34 +1000

    Cache qualified-name lookups

--- src/IsOK.php
+++ src/IsOK.php
@@ -8,7 +8,8 @@ abstract class IsOK
     use IsOKToolboxTrait;
 
     protected static $v3QualifiedNameCache = [];
-    protected static $v3QualifiedNameRegex;
+    protected static $v3QualifiedNameRegex =
+        '/[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}(\.[\p{L}\p{Nl}][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}\p{Cf}]{0,}){0,}/';
 
     abstract public function isOK(&$msg = null);
 
