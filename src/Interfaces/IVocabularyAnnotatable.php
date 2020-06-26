d4f372aa00921f47c1aa3ad0f83cfcf19e78ce87
c-harris <c.harris@communicationinnovations.com.au>
2020-06-26 11:27:35 +1000

    add @mixin VocabularyAnnotatableHelpers for IVocabularyAnnotatable

--- src/Interfaces/IVocabularyAnnotatable.php
+++ src/Interfaces/IVocabularyAnnotatable.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 
 namespace AlgoWeb\ODataMetadata\Interfaces;
 
+use AlgoWeb\ODataMetadata\Helpers\VocabularyAnnotatableHelpers;
+
 /**
  * Interface IEdmVocabularyAnnotatable.
  *
  * Represents an element that can be targeted by Vocabulary Annotations
  *
  * @package AlgoWeb\ODataMetadata\MetadataV3\Interfaces
+ * @mixin VocabularyAnnotatableHelpers
  */
 interface IVocabularyAnnotatable extends IEdmElement
 {
