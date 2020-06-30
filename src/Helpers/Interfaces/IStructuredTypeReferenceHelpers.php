0b2c4c9cf1ae6cf1c4dfff75c362586352a9cba9
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-01 03:37:43 +1000

    Null-guard structuredDefinition calls in StructuredTypeReferenceHelpers

--- src/Helpers/Interfaces/IStructuredTypeReferenceHelpers.php
+++ src/Helpers/Interfaces/IStructuredTypeReferenceHelpers.php
@@ -38,9 +38,9 @@ interface IStructuredTypeReferenceHelpers
     /**
      * Returns the base type of the definition of this reference.
      *
-     * @return IStructuredType the base type of the definition of this reference
+     * @return IStructuredType|null the base type of the definition of this reference
      */
-    public function BaseType(): IStructuredType;
+    public function BaseType(): ?IStructuredType;
 
     /**
      * Gets all structural properties declared in the definition of this reference.
@@ -59,8 +59,8 @@ interface IStructuredTypeReferenceHelpers
     /**
      * Finds a property from the definition of this reference.
      *
-     * @param  string    $name name of the property to find
-     * @return IProperty The requested property if it exists. Otherwise, null.
+     * @param  string         $name name of the property to find
+     * @return IProperty|null The requested property if it exists. Otherwise, null.
      */
-    public function FindProperty(string $name): IProperty;
+    public function FindProperty(string $name): ?IProperty;
 }
