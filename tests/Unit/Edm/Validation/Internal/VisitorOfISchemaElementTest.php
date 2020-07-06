eafebdb0579c40ed3da13afd033d0ed65e18e8e8
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-06 16:22:15 +1000

    Simplify VisitorOfISchemaElement internals

--- tests/Unit/Edm/Validation/Internal/VisitorOfISchemaElementTest.php
+++ tests/Unit/Edm/Validation/Internal/VisitorOfISchemaElementTest.php
@@ -8,9 +8,15 @@
 
 namespace AlgoWeb\ODataMetadata\Tests\Unit\Edm\Validation\Internal;
 
+use AlgoWeb\ODataMetadata\Edm\Validation\EdmError;
+use AlgoWeb\ODataMetadata\Edm\Validation\EdmErrorCode;
 use AlgoWeb\ODataMetadata\Edm\Validation\Internal\InterfaceValidator\VisitorOfISchemaElement;
 use AlgoWeb\ODataMetadata\Enums\SchemaElementKind;
+use AlgoWeb\ODataMetadata\Interfaces\IEntityContainer;
+use AlgoWeb\ODataMetadata\Interfaces\IFunction;
 use AlgoWeb\ODataMetadata\Interfaces\ISchemaElement;
+use AlgoWeb\ODataMetadata\Interfaces\ISchemaType;
+use AlgoWeb\ODataMetadata\Interfaces\IValueTerm;
 use AlgoWeb\ODataMetadata\Tests\TestCase;
 use Mockery as m;
 
@@ -29,5 +35,66 @@ class VisitorOfISchemaElementTest extends TestCase
         $result = $foo->Visit($item, $followUp, $references);
         $this->assertTrue(is_array($result));
         $this->assertEquals(1, count($result));
+    }
+
+    public function testVisitBadSchemaElementKindNonNullNamespace()
+    {
+        $item = m::mock(ISchemaElement::class)->makePartial();
+        $item->shouldReceive('getNamespace')->andReturn('namespace')->atLeast(1);
+        $item->shouldReceive('getSchemaElementKind')->andReturn(m::mock(SchemaElementKind::class)->makePartial())->atLeast(1);
+
+        $foo = new VisitorOfISchemaElement();
+        $followUp   = [];
+        $references = [];
+
+        $result = $foo->Visit($item, $followUp, $references);
+        $this->assertTrue(is_array($result));
+        $this->assertEquals(1, count($result));
+        /** @var EdmError $error */
+        $error = $result[0];
+
+        $errorCode = EdmErrorCode::InterfaceCriticalEnumPropertyValueOutOfRange();
+        $this->assertEquals($errorCode, $error->getErrorCode());
+        $expected = 'that is not a valid enum member.';
+        $this->assertContains($expected, $error->getErrorMessage());
+    }
+
+    public function schemaElementKindProvider(): array
+    {
+        $result = [];
+        $result[] = [SchemaElementKind::TypeDefinition(), ISchemaType::class];
+        $result[] = [SchemaElementKind::Function(), IFunction::class];
+        $result[] = [SchemaElementKind::ValueTerm(), IValueTerm::class];
+        $result[] = [SchemaElementKind::EntityContainer(), IEntityContainer::class];
+
+        return $result;
+    }
+
+    /**
+     * @dataProvider schemaElementKindProvider
+     *
+     * @param SchemaElementKind $kind
+     * @param string $mustImplement
+     */
+    public function testVisitGoodSchemaElementKindNonNullNamespace(SchemaElementKind $kind, string $mustImplement)
+    {
+        $item = m::mock(ISchemaElement::class)->makePartial();
+        $item->shouldReceive('getNamespace')->andReturn('namespace')->atLeast(1);
+        $item->shouldReceive('getSchemaElementKind')->andReturn($kind)->atLeast(1);
+
+        $foo = new VisitorOfISchemaElement();
+        $followUp   = [];
+        $references = [];
+
+        $result = $foo->Visit($item, $followUp, $references);
+        $this->assertTrue(is_array($result));
+        $this->assertEquals(1, count($result));
+        /** @var EdmError $error */
+        $error = $result[0];
+
+        $errorCode = EdmErrorCode::InterfaceCriticalKindValueMismatch();
+        $this->assertEquals($errorCode, $error->getErrorCode());
+        $expected = 'property must implement \''. $mustImplement. '\' interface.';
+        $this->assertContains($expected, $error->getErrorMessage());
     }
 }
