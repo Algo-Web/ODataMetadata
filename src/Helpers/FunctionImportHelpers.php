bdf943526dd8aa3574257ab07107eb9bddf41c4e
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-18 23:03:28 +1000

    Wring out FunctionImportEntityTypeDoesNotMatchEntitySet

--- src/Helpers/FunctionImportHelpers.php
+++ src/Helpers/FunctionImportHelpers.php
@@ -26,7 +26,7 @@ trait FunctionImportHelpers
      * @param  IEntitySet $entitySet the static entity set of the function import
      * @return bool       true if the entity set expression of the functionImport contains a static reference to an IEntitySet, otherwise false
      */
-    public function TryGetStaticEntitySet(IEntitySet &$entitySet): bool
+    public function TryGetStaticEntitySet(IEntitySet &$entitySet = null): bool
     {
         /** @var IFunctionImport $this */
         $entitySetReference = $this->getEntitySet();
@@ -38,13 +38,18 @@ trait FunctionImportHelpers
      * Analyzes IFunctionImport::EntitySet expression and returns a relative path to an IEntitySet if available.
      * The path starts with the parameter and may have optional sequence of NavigationProperty and type casts segments.
      *
-     * @param  IModel                $model     the model containing the function import
-     * @param  IFunctionParameter    $parameter the function import parameter from which the relative entity set path starts
-     * @param  INavigationProperty[] $path      the optional sequence of navigation properties
-     * @return bool                  true if the entity set expression of the functionImport contains a relative path an IEntitySet, otherwise false
+     * @param  IModel                     $model     the model containing the function import
+     * @param  IFunctionParameter|null    $parameter the function import parameter from which the relative entity set
+     *                                               path starts
+     * @param  INavigationProperty[]|null $path      the optional sequence of navigation properties
+     * @return bool                       true if the entity set expression of the functionImport contains a relative
+     *                                    path an IEntitySet, otherwise false
      */
-    public function TryGetRelativeEntitySetPath(IModel $model, IFunctionParameter &$parameter, array &$path): bool
-    {
+    public function TryGetRelativeEntitySetPath(
+        IModel $model,
+        IFunctionParameter &$parameter = null,
+        array &$path = null
+    ): bool {
         /**
          * @var IFunctionImport $this
          */
