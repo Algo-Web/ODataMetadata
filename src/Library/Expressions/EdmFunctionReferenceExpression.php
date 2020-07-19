0cab5f5262115f9f9de04d7171f57ae254ed74ba
c-harris <c.harris@communicationinnovations.com.au>
2020-07-19 13:48:31 +1000

    provide function Reference Expression

---
+++ src/Library/Expressions/EdmFunctionReferenceExpression.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace AlgoWeb\ODataMetadata\Library\Expressions;
+
+
+use AlgoWeb\ODataMetadata\Enums\ExpressionKind;
+use AlgoWeb\ODataMetadata\Interfaces\Expressions\IFunctionReferenceExpression;
+use AlgoWeb\ODataMetadata\Interfaces\IFunction;
+use AlgoWeb\ODataMetadata\Library\EdmElement;
+
+/**
+ * Represents an EDM function reference expression.
+ *
+ * @package AlgoWeb\ODataMetadata\Library\Expressions
+ */
+class EdmFunctionReferenceExpression extends EdmElement implements IFunctionReferenceExpression
+{
+    /**
+     * @var IFunction
+     */
+    private $referencedFunction;
+
+    /**
+     * Initializes a new instance of the EdmFunctionReferenceExpression class.
+     * @param IFunction $referencedFunction Referenced function
+     */
+    public  function __construct(IFunction $referencedFunction)
+    {
+        $this->referencedFunction = $referencedFunction;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExpressionKind(): ExpressionKind
+    {
+        return ExpressionKind::FunctionReference();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getReferencedFunction(): IFunction
+    {
+        return $this->referencedFunction;
+    }
+}
