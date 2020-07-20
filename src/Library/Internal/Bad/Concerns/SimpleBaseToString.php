26e3ea997b0306c5171de31c7bd3cd83f6108331
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-20 15:47:05 +1000

    Repoint SimpleBaseToString to use ICheckable

--- src/Library/Internal/Bad/Concerns/SimpleBaseToString.php
+++ src/Library/Internal/Bad/Concerns/SimpleBaseToString.php
@@ -6,18 +6,20 @@ declare(strict_types=1);
 namespace AlgoWeb\ODataMetadata\Library\Internal\Bad\Concerns;
 
 use AlgoWeb\ODataMetadata\Helpers\ToTraceString;
+use AlgoWeb\ODataMetadata\Interfaces\ICheckable;
 use AlgoWeb\ODataMetadata\Interfaces\IEdmElement;
 
 trait SimpleBaseToString
 {
     public function __toString(): string
     {
-        /** @var SimpleICheckable|IEdmElement $self */
+        /** @var ICheckable|IEdmElement $self */
         $self = $this;
-        assert(count($self->errors) !== 0);
-        $error = $self->errors[0];
-        assert($error !== null, 'error != null');
-        $prefix = $error != null ?
+        $errors = $self->getErrors();
+        assert(count($errors) !== 0);
+        $error = $errors[0];
+        assert(null !== $error, 'error != null');
+        $prefix = null !== $error ?
             strval($error->getErrorCode()->getKey()) . ':' . $error->getErrorCode()->getValue() . ':' : '';
         return $prefix . ToTraceString::ToTraceString($self);
     }
