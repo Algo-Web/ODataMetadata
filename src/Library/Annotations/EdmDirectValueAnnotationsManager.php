6ff61b0eb730a432c4981d507ff1865d2c380b24
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-22 21:08:10 +1000

    Fix casing, lines

--- src/Library/Annotations/EdmDirectValueAnnotationsManager.php
+++ src/Library/Annotations/EdmDirectValueAnnotationsManager.php
@@ -112,7 +112,12 @@ class EdmDirectValueAnnotationsManager implements IDirectValueAnnotationsManager
     {
         /** @var IDirectValueAnnotationBinding $annotation */
         foreach ($annotations as $annotation) {
-            $this->SetAnnotationValue($annotation->getElement(), $annotation->getNamespaceUri(), $annotation->getName(), $annotation->getValue());
+            $this->SetAnnotationValue(
+                $annotation->getElement(),
+                $annotation->getNamespaceUri(),
+                $annotation->getName(),
+                $annotation->getValue()
+            );
         }
         return $this;
     }
