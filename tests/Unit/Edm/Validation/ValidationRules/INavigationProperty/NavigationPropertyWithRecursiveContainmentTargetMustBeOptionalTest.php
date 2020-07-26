59c2c221e4f172b27f6ac2d7df6237700318c8f0
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-07-27 04:16:11 +1000

    Wring out and simplify NavigationPropertyDependentPropertiesMustBelongToDependentEntity

---
+++ tests/Unit/Edm/Validation/ValidationRules/INavigationProperty/NavigationPropertyWithRecursiveContainmentTargetMustBeOptionalTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 27/07/20
+ * Time: 4:00 AM
+ */
+
+namespace AlgoWeb\ODataMetadata\Tests\Unit\Edm\Validation\ValidationRules\INavigationProperty;
+
+class NavigationPropertyWithRecursiveContainmentTargetMustBeOptionalTest
+{
+
+}
